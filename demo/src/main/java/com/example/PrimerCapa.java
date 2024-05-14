4d91fc3cf9b118594accd5090a7c1887760dd792
Matias Siano <matias.siano@outlook.com>
2024-05-14 10:21:27 -0300

    Update PrimerCapa.java

--- demo/src/main/java/com/example/PrimerCapa.java
+++ demo/src/main/java/com/example/PrimerCapa.java
@@ -1,12 +1,19 @@
 package com.example;
 
 import javax.swing.JPanel;
-import java.awt.Graphics; // Import the Graphics class
+import java.awt.*; // Import the Graphics class
+import java.awt.geom.*;
 
 public class PrimerCapa extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g); // llamar al método paintComponent de la clase padre
         g.drawString("Hola Mundo", 200, 100); // Dibujar un texto y posicionarlo en la ventana con coordenadas (200,
                                               // 100)
+        Graphics2D g2 = (Graphics2D) g; // Crear un objeto Graphics2D
+        Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 150); // Crear un rectángulo
+        g2.draw(rectangulo); // Dibujar el rectángulo
+        Ellipse2D elipse = new Ellipse2D.Double(); // Crear una elipse
+        elipse.setFrame(rectangulo); // Establecer el tamaño de la elipse al tamaño del rectángulo
+        g2.draw(elipse); // Dibujar la elipse
     }
 }
