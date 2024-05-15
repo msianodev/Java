a2e06d5e216245a2755a8d1b54fd1736f90ef02a
Matias Siano <matias.siano@outlook.com>
2024-05-15 10:29:28 -0300

    utilizando imagenes

--- demo/src/main/java/com/example/Desarrollo.java
+++ demo/src/main/java/com/example/Desarrollo.java
@@ -8,10 +8,12 @@ public class Desarrollo extends JFrame {
         setSize(600, 600); // Establecer el tamaño de la ventana
         setLocation(400, 200); // Establecer la posición de la ventana
         setTitle("Ventana de desarrollo"); // Establecer el título de la ventana
-        PrimerCapa capa1 = new PrimerCapa(); // Crear una capa
-        add(capa1); // Agregar la capa a la ventana
-        SegundaCapaColor capa2 = new SegundaCapaColor(); // Crear una capa
-        add(capa2); // Agregar la capa a la ventana
+        // PrimerCapa capa1 = new PrimerCapa(); // Crear una capa
+        // add(capa1); // Agregar la capa a la ventana
+        // SegundaCapaColor capa2 = new SegundaCapaColor(); // Crear una capa
+        // add(capa2); // Agregar la capa a la ventana
+        TerceraCapaImagenes capa3 = new TerceraCapaImagenes(); // Crear una capa
+        add(capa3); // Agregar la capa a la ventana
 
     }
 
